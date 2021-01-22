a0dd8fecf868b490faa59c42737c817b4a967d08
Алексей Перепелкин <catdog50rus@gmail.com>
2021-01-22 15:43:50 +0300

    Небольшая доработка сервиса получения отчета. Добавление комментариев #20

--- Reports/Services/SalaryReportService/SalaryReportService.cs
+++ Reports/Services/SalaryReportService/SalaryReportService.cs
@@ -1,55 +1,51 @@
 ﻿using Catdog50RUS.EmployeesAccountingSystem.Data.Services;
-using Catdog50RUS.EmployeesAccountingSystem.Models;
 using Catdog50RUS.EmployeesAccountingSystem.Models.Employees;
 using Catdog50RUS.EmployeesAccountingSystem.Reports.Models.SalaryReport;
 using System;
-using System.Linq;
 
 namespace Catdog50RUS.EmployeesAccountingSystem.Reports.Services.SalaryReportService
 {
     public class SalaryReportService : ISalaryReportService
     {
+        /// <summary>
+        /// Инжекция сервиса получения логов
+        /// </summary>
         private readonly ICompletedTaskLogsService _taskLogsService;
 
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="taskLogsService"></param>
         public SalaryReportService(ICompletedTaskLogsService taskLogsService)
         {
             _taskLogsService = taskLogsService ?? throw new ArgumentNullException(nameof(taskLogsService));
         }
-        
 
+        #region Интерфейс
 
+        /// <summary>
+        /// Получить отчет по зарплате сотрудника
+        /// </summary>
+        /// <param name="employee">Сотрудник</param>
+        /// <param name="period">Период</param>
+        /// <returns>Возвращает отчет</returns>
         public SalaryReport GetEmployeeSalaryReport(BaseEmployee employee, (DateTime firstDate, DateTime lastDate) period)
         {
-            try
-            {
-                IQueryable<CompletedTaskLog> employeeTasksLogList = _taskLogsService.GetEmployeeTaskLogs(employee.Id,
-                                                                                              period.firstDate,
-                                                                                              period.lastDate)
-                                                                         .Result
-                                                                         .AsQueryable();
-                //Result
-                var salaryReport = new SalaryReport(period.firstDate, period.lastDate, employee, employeeTasksLogList)
-                {
-                    Header = $"Отчет по Заработной плате cотрудника: {employee}\nВ период с {period.firstDate:dd.MM.yyyy} по {period.lastDate:dd.MM.yyyy}\n",
-                    TotalSalary = employee.CalculateSamary(employeeTasksLogList)
-                };
-
-                return salaryReport;
-            }
-            catch (Exception)
-            {
-
+            //Получаем список логов
+            var employeeTasksLogList = _taskLogsService.GetEmployeeTaskLogs(employee.Id, period.firstDate, period.lastDate).Result;
+            if (employeeTasksLogList == null)
                 return null;
-            }
-            
 
-            
-
+            //Result
+            var salaryReport = new SalaryReport(period.firstDate, period.lastDate, employee, employeeTasksLogList)
+            {
+                Header = $"Отчет по Заработной плате cотрудника: {employee}\nВ период с {period.firstDate:dd.MM.yyyy} по {period.lastDate:dd.MM.yyyy}\n",
+                TotalSalary = employee.CalculateSamary(employeeTasksLogList)
+            };
+            return salaryReport;
         }
-
-
-
-
         
+        #endregion
+
     }
 }

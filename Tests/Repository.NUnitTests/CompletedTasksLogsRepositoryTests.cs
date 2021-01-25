8acd4dec85e80c26615c3caaa214a63881482cb6
Алексей Перепелкин <catdog50rus@gmail.com>
2021-01-25 17:15:30 +0300

    #24 Реализация тестов получения логов Правка репозитория логов

--- Tests/Repository.NUnitTests/CompletedTasksLogsRepositoryTests.cs
+++ Tests/Repository.NUnitTests/CompletedTasksLogsRepositoryTests.cs
@@ -15,13 +15,14 @@ namespace Repository.NUnitTests
     {
         private ICompletedTasksLogRepository _repository;
         private List<CompletedTaskLog> _completedTaskLogs;
+        Guid id1 = Guid.NewGuid();
+        Guid id2 = Guid.NewGuid();
+        Guid id3 = Guid.NewGuid();
+
         [SetUp]
         public void TestsSetup()
         {
             _repository = new FileCSVCompletedTasksLogRepository();
-            var id1 = Guid.NewGuid();
-            var id2 = Guid.NewGuid();
-            var id3 = Guid.NewGuid();
 
             _completedTaskLogs = new List<CompletedTaskLog>
             {
@@ -34,6 +35,10 @@ namespace Repository.NUnitTests
                 new CompletedTaskLog(Guid.NewGuid(), id3, DateTime.Now.Date.AddDays(-3), 9, "TestTask5"),
                 new CompletedTaskLog(Guid.NewGuid(), id3, DateTime.Now.Date.AddDays(-2), 3, "TestTask6")
             };
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), FileCSVSettings.TASKSLOGS_FILENAME);
+            if (File.Exists(path))
+                File.Delete(path);
         }
 
         #region Insert
@@ -42,10 +47,6 @@ namespace Repository.NUnitTests
         [Test]
         public void A__InsertTimeLog_ShouldReturnTaskLogsList()
         {
-            var path = Path.Combine(Directory.GetCurrentDirectory(), FileCSVSettings.TASKSLOGS_FILENAME);
-            if (File.Exists(path))
-                File.Delete(path);
-
             var id = Guid.NewGuid();
             var log = new CompletedTaskLog(Guid.NewGuid(), id, DateTime.Now.Date.AddDays(-2), 3, "TestTask6");
 
@@ -72,12 +73,92 @@ namespace Repository.NUnitTests
 
         #region GetAll
 
+        //Получение логов выполненных задач в заданный период
+        [Test]
+        public void C_GetAllCompletedTaskLogsInPeriod_ShoulReturnTaskLoagsList()
+        {
+            foreach (var l in _completedTaskLogs)
+            {
+                _repository.InsertCompletedTaskAsync(l);
 
+            }
 
+            var startDate = DateTime.Now.AddDays(-10);
+            var stopDate = DateTime.Now;
+
+            var result = _repository.GetCompletedTasksListInPeriodAsync(startDate, stopDate).Result.ToList();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(_completedTaskLogs.Count, result.Count);
+            Assert.AreEqual(40d, result.Sum(t => t.Time));
+        }
+
+        //Получение логов выполненных задач в заданный период, return null
+        [TestCase("2021.02.25","2021.01.25")]
+        [TestCase("2021.01.01", "2021.01.05")]
+        [TestCase("2021.01.20", "2021.01.05")]
+        public void D_GetAllCompletedTaskLogsInPeriod_ShoulReturnNull(string strFirstDay, string strLastDay)
+        {
+            foreach (var l in _completedTaskLogs)
+            {
+                _repository.InsertCompletedTaskAsync(l);
+
+            }
+
+            var startDate = DateTime.Parse(strFirstDay);
+            var stopDate = DateTime.Parse(strLastDay);
+
+            var result = _repository.GetCompletedTasksListInPeriodAsync(startDate, stopDate).Result;
+
+            Assert.IsNull(result);
+        }
 
         #endregion
 
+        #region GetByEmployee
 
+        //Получение логов выполненных задач в заданный период
+        [Test]
+        public void E_GetAllCompletedTaskLogsByEmployeeInPeriod_ShoulReturnTaskLoagsList()
+        {
+            foreach (var l in _completedTaskLogs)
+            {
+                _repository.InsertCompletedTaskAsync(l);
 
+            }
+
+            var startDate = DateTime.Now.AddDays(-10);
+            var stopDate = DateTime.Now;
+
+            var result = _repository.GetCompletedTasksListByEmployeeAsync(id1, startDate, stopDate).Result.ToList();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(13d, result.Sum(t => t.Time));
+        }
+
+        //Получение логов выполненных задач в заданный период, return null
+        [TestCase("2021.02.25", "2021.01.25")]
+        [TestCase("2021.01.01", "2021.01.05")]
+        [TestCase("2021.01.20", "2021.01.05")]
+        [TestCase("2021.01.20", "2021.01.25")] //Вариант корректный по времени, но неверный id
+        public void F_GetAllCompletedTaskLogsInPeriod_ShoulReturnNull(string strFirstDay, string strLastDay)
+        {
+            foreach (var l in _completedTaskLogs)
+            {
+                _repository.InsertCompletedTaskAsync(l);
+
+            }
+
+            var id = Guid.NewGuid();
+            var startDate = DateTime.Parse(strFirstDay);
+            var stopDate = DateTime.Parse(strLastDay);
+
+            var result = _repository.GetCompletedTasksListByEmployeeAsync(id, startDate, stopDate).Result;
+
+            Assert.IsNull(result);
+        }
+
+        #endregion
     }
 }
